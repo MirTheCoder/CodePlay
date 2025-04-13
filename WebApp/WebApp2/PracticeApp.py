1c572a1b47a0197c5d912ab5bcf6a0d666d36f7c
MirTheCoder <mokoli142@gmail.com>
2025-04-13 18:06:28 -0400

    We have started working on a new web applicaiton that has sessions, user login, and will potentially have a database to save data that is specific to the user who  is logged in.

---
+++ WebApp/WebApp2/PracticeApp.py
@@ -0,0 +1,41 @@
+from flask import Flask, redirect, url_for,render_template, request, session
+from flask import flash
+from datetime import timedelta
+import sqlalchemy
+app = Flask(__name__)
+app.secret_key = "QETYUIPKHGDAVXBM10928374"
+app.permanent_session_lifetime = timedelta(minutes = 13)
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True;
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            same = session["user"]
+            flash(f"Hey {same}, You are already logged in", "info")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        flash("Login successful", "info")
+        return render_template("user.html", text = user)
+    else:
+        flash("You are not logged in", "info")
+        return redirect(url_for("login"))
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been successfully logged out {user}!", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
+if __name__ == "__main__":
+    app.run(debug = True)
