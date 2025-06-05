ff0eb81b5acb18be7e213193627dd202ac9a3b95
Miracle Okoli <mokoli142@gmal.com>
2025-06-05 00:51:20 -0400

    made a few edits and comments to my webapp project

--- WebApp/PracticeApp.py
+++ WebApp/PracticeApp.py
@@ -2,16 +2,19 @@ from flask import Flask, redirect, url_for,render_template, request, session
 from flask import flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+import os
 import sqlalchemy
 app = Flask(__name__)
 app.secret_key = "QETYUIPKHGDAVXBM10928374"
+#We use this in order to have the user once logged in, have their session logged in for 13 minutes, this means that
+#for 13 minutes they will be logged in already or auto logged in
 app.permanent_session_lifetime = timedelta(minutes = 13)
 db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
-
+#Here is our database where we will store the image of each user
 class users(db.Model):
     #Each row will be given a unique id, this is how we store data in the database view rows and columns
     _id = db.Column("id", db.Integer, primary_key=True)
@@ -20,7 +23,7 @@ class users(db.Model):
     email = db.Column(db.String(100))
     age = db.Column(db.Integer)
     pswd = db.Column(db.String(100))
-    image_file = db.Column(db.String(200), default="default.jpg")
+    image_file = db.Column(db.String(20000000000000000000))
     #Used to define each person or ID within our database
     def __init__(self, uname, email, age, pswd, image):
         self.uname = uname
@@ -40,21 +43,28 @@ def login():
     valid = False
     text = "This account does not exist, please enter a valid account or create and account"
     if request.method == "POST":
+        #The session timer starts
         session.permanent = True
+        #Here we will retrive the username and password input by the user in the sign-in form if their session has expired,
+        #and they have to do a post request or fill out a form to get in
         user = request.form["nm"]
         word = request.form["ps"]
         #session["user"] = user
         found_user = users.query.filter_by(uname = user).first()
         found_pswd = users.query.filter_by(pswd = word).first()
         if found_user and found_pswd:
+            #If the username and password are found within the user database, tehn we will give the user access to the user page
             valid = False
             session["user"] = found_user.uname
             session["password"] = found_user.pswd
             return redirect(url_for("user"))
         else:
+            #if not true that both the username and password are found in the database, then we will
+            #tell the suer to either try again or make an account
             valid = True
             return render_template("login.html", one = valid, two = text)
     else:
+        #If the user is already in session, then the application will let them know that they are already logged in
         if "user" in session:
             valid = False
             same = session["user"]
@@ -66,22 +76,27 @@ def login():
 
 @app.route("/user", methods=["POST", "GET"])
 def user():
+    #If the user is able to log in and the session starts, then the user will be notified that it was a success
     if "user" in session:
         user = session["user"]
         flash("Login successful", "info")
+        #In the user page, the user can add their email and it will be saved within the database for their specific user
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
             found_user = users.query.filter_by(name=user).first()
             found_user.email = email
             db.session.commit()
+            #Message to notify the user that the email was successfully saved
             flash("Email was saved!")
             flash(f"Email: {email}")
         else:
             if "email" in session:
                 email = session["email"]
-        return render_template("user.html", text = user,)
+        return render_template("user.html", text = user)
     else:
+        #If the user is not in session and has been logged out, then they will be denied the chance to access users and
+        #will be returned to the login page to start up their session or create a new account
         flash("You are not logged in", "info")
         return redirect(url_for("login"))
 @app.route("/details")
@@ -89,37 +104,51 @@ def details():
     if "user" in session:
         username = session["user"]
         found_user = users.query.filter_by(uname = username).first()
-        hold = found_user.image_file
-        email = found_user.email
-        age = found_user.age
-    return render_template("details.html", photo = hold, zone = email, num = age)
+        email = session["email"]
+        age = session["age"]
+        picture = session["image"]
+    else:
+        flash("You are not logged in","info")
+        return redirect(url_for("login"))
+    return render_template("details.html", photo = picture, email = email, num = age)
 @app.route("/create",methods = ["POST", "GET"])
 def create():
+    #This allows the user to create an account and have all their details stored within the session
+    #basically, their data will be saved in the session
     if request.method == "POST":
         session.permanent = True
         user = request.form["nm"]
         word = request.form["ps"]
         email = request.form["em"]
         age = request.form["ag"]
-        image = request.form["png"]
-        usr = users(user,email,age,word,image)
+        pic = request.files["png"]
         session["user"] = user
         session["word"] = word
         session["email"] = email
         session["age"] = age
-        session["image"] = image
+        picture = os.path.join("static", pic.filename)
+        pic.save(picture)
+        session["image"] = picture
+        usr = users(user, email, age, word, picture)
+        #add the new account to the database
         db.session.add(usr)
         db.session.commit()
+        #Sends the new user to the user page
         return redirect(url_for("user"))
+    #Will return the user to the create template if they do not submit a post request
     return render_template("create.html")
     # found_user = users.query.filter_by(name = user).first()
 @app.route("/logout")
 def logout():
+    #Will only execute if the user is already in session
     if "user" in session:
         user = session["user"]
         flash(f"You have been successfully logged out {user}!", "info")
+    #This will cancel the session of the user and also remove any saved information under the user in that session as
+    #well
     session.pop("user", None)
     session.pop("email", None)
+    #Returns the user back to the login page
     return redirect(url_for("login"))
 if __name__ == "__main__":
     with app.app_context():
